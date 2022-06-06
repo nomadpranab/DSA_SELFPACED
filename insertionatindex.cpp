9f8eaa39db6363f4797da1f428bbb0e25535343b
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-06 21:08:25 +0530

    -

--- insertionatindex.cpp
+++ insertionatindex.cpp
@@ -4,23 +4,33 @@ using namespace std;
 
 int main()
 {   
-    int pos=0,n=0;
-    int arr[5];
+    int n;
+    int pos;
+    
+    int arr[5]={1,2,7,4};
     cout<<"Enter position to insert"<<endl;
     cin>>pos;
     cout<<"Enter the no you want to insert"<<endl;
     cin>>n;
     int index=pos-1;
+    cout<<"n="<<n<<endl;
 
-    for(int i=n-1;i>=index;i--)
+   
+    
+
+    for(int i=4;i>=index;i--)
     {
         arr[i+1]=arr[i];
     }
+    cout<<"n="<<n<<endl;
+
     arr[index]=n;
 
-    for(int i=0;i<=n-1;i++)
+    for(int i=0;i<5;i++)
     {
         cout<<arr[i];
     }
-    return 0;
+
+    
+    
 }
