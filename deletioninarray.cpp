3bf46a9e3abfe4383241458d91c95c3e8748c31d
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-07 20:36:04 +0530

    -

--- deletioninarray.cpp
+++ deletioninarray.cpp
@@ -1,1 +1,29 @@
--
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[4]={1,2,3,4};
+    int temp,n,index,o=0;
+
+    cout<<"Enter position to be deleted"<<endl;
+    cin>>n;
+    index=n-1;
+
+    for(int i=index;i<4;i++)
+    {
+    
+        arr[i]=arr[i+1];
+        
+    }
+    
+    arr[3]=0;
+    
+
+    for(int i=0;i<4;i++)
+    {
+        cout<<arr[i]<<"   ";
+    }
+
+    return 0;
+
+}
