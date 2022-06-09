a560ac48a23b6d1e9a4fb6611a87491c3e14af06
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-09 21:07:50 +0530

    -

--- moveZeroesToEnd.cpp
+++ moveZeroesToEnd.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int  main()
+{
+    int arr[4]={1,0,0,4};
+    int temp=0;
+    for(int i=0;i<4;i++)
+    {
+        if(arr[i]==0)
+        {
+            for(int j=i+1;j<4;j++)
+            {
+                if(arr[j]!=0)
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+    }
+    
+    for(int i=0;i<4;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
