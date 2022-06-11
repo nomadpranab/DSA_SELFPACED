63b0bce0c4861c07d2449125c3d374c2cc2e2f73
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-11 21:08:04 +0530

    o

--- leaderInArray.cpp
+++ leaderInArray.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int arr[8]={1,2,3,4,5,3,2,1};
+    int curr_leader;
+    curr_leader=arr[7];
+    cout<<curr_leader<<" ";
+    for(int i=7;i>0;i--)
+    {
+        if(arr[i]>curr_leader)
+        {
+            curr_leader=arr[i];
+            cout<<curr_leader<<" ";
+        }
+    }
+}
