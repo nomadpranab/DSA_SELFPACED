51bd913cd2f58b4a117e4da71dbca4af4b8ffbef
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-19 11:37:53 +0530

    --

--- BinarySearchUsingRecursion.cpp
+++ BinarySearchUsingRecursion.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int bsearch(int arr[],int low,int high,int x)
+{
+    int mid=(low+high)/2;
+    if(arr[mid]==x)
+    {
+        return mid;
+    }
+    else if(arr[mid]>x)
+    {
+        return bsearch(arr,low,mid-1,6);
+    }
+    else
+    {
+        return bsearch(arr,mid+1,high,6);
+    }
+}
+
+int main()
+{
+    int arr[5]={5,6,7,8,9};
+    cout<<bsearch(arr,0,4,6);
+}
