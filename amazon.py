be1a048910c1a175ea9d36587cf26bea313c3709
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-19 10:27:11 +0530

    -

---
+++ amazon.py
@@ -0,0 +1,22 @@
+ray=""
+ben=""
+kelvin=""
+ray=input()
+
+#move1 * 3
+ch=ray[0]
+ben+=ch
+ray=ray[1:]
+
+ch=ray[0]
+ben+=ch
+ray=ray[1:]
+ch=ray[0]
+ben+=ch
+ray=ray[1:]
+
+print(ray)
+print(ben)
+
+
+    
