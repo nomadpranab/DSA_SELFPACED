1b009132734db82e0c8ca6b9deeed84c6e79f22c
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-19 10:31:50 +0530

    --

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int bSearch(int arr[],int n,int x)
+{
+    int low=0,mid=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        if(arr[mid]==x)
+        {
+            return mid;
+        }
+        else if(arr[mid]>x)
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+}
+
+int main(){
+    int arr[6]={1,2,3,4,5,6};
+    cout<<bSearch(arr,6,4);
+}
