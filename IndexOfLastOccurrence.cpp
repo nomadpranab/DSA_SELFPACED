79c95f7129d9c1360bea309d954ae7a5d70ba311
PRANAB PATRA <pranabpatra520@gmail.com>
2022-06-20 11:10:51 +0530

    s

--- IndexOfLastOccurrence.cpp
+++ IndexOfLastOccurrence.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,1,1,1,6};
+    for(int i=5;i>0;i--)
+    {
+        if(arr[i]==1)
+        {
+            cout<<i;
+            break;
+        }
+    }
+}
